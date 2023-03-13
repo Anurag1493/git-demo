9a8a5745d022c862a8355d3eee34efa23a1a40bc
Anurag <anurg14k@gmail.com>
2023-03-13 04:19:26 +0000

    Devops changes commited

--- demo.java
+++ demo.java
@@ -2,5 +2,8 @@ public class Demo{
 	public static void main(strig args[]){
 		// print statement for hello world
 		system.out.println("Hello world");
+		//print statement for Devops:
+		system.out.println("Welcome to Devops");
+
         }
 } 
