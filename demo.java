7660e63390c3bde5c9cd8f859e20a52a53efe442
Anurag <anurg14k@gmail.com>
2023-03-14 12:15:44 +0000

    devB changed the code

--- demo.java
+++ demo.java
@@ -1,6 +1,6 @@
 public class Demo{
 	public static void main(strig args[]){
-		// print statement for hello world
+		// DevB code for Hello world
 		system.out.println("Hello world");
 		//print statement for Devops
 	        system.out.println("welcome to DevOps");
